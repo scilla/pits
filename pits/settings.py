8ec5d023ce649e87a5c69172ff2b2e5a5471888a
scilla <sr.cilla@gmail.com>
2024-05-03 09:41:31 +0200

    Clean-up

--- pits/settings.py
+++ pits/settings.py
@@ -18,7 +18,6 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
 	"chat.apps.ChatConfig",
-	"ai.apps.AiConfig",
     "frontend.apps.FrontendConfig",
     "django.contrib.admin",
     "django.contrib.auth",
