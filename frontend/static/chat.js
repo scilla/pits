4e6ea2000899dd905196d5849591931e7d8e1b6d
scilla <sr.cilla@gmail.com>
2024-05-06 13:44:53 +0200

    Refactor message labels, add scroll observer

--- frontend/static/chat.js
+++ frontend/static/chat.js
@@ -1,8 +1,6 @@
 const HUMAN_LABEL = "[Human]: ";
 const PITS_LABEL = "[Pits]: ";
 
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
-
 const getChatId = () => {
   const parsedUrl = new URL(window.location.href);
   const path = parsedUrl.pathname.endsWith("/")
@@ -11,26 +9,29 @@ const getChatId = () => {
   return path.split("/").pop();
 };
 
-const appendMessage = (content, label, container, delay) => {
+const appendMessage = (content, is_human, container, delay) => {
   const newMessageNode = document.createElement("div");
-  const wrapper = document.getElementById("wrapper");
   const typeSpeed = 20;
+  let label = PITS_LABEL;
+
+  if (is_human) {
+    label = HUMAN_LABEL;
+    newMessageNode.classList.add("human");
+  }
+
   container.appendChild(newMessageNode);
   if (delay) {
     for (let i = 0; i < content.length; i++) {
       setTimeout(() => {
         newMessageNode.textContent = label + content.slice(0, i + 1);
-        wrapper.scrollTo(0, wrapper.scrollHeight);
       }, i * typeSpeed);
     }
     setTimeout(() => {
       showInputArea();
-      wrapper.scrollTo(0, wrapper.scrollHeight);
     }, content.length * typeSpeed);
   } else {
     newMessageNode.textContent = label + content;
   }
-  wrapper.scrollTo(0, wrapper.scrollHeight);
 };
 
 const fetchJson = async (url, options = {}) => {
@@ -47,11 +48,7 @@ const getMessages = async (chat_id, container) => {
   const data = await fetchJson(`/chat/${chat_id}/messages/`);
   if (data) {
     data.forEach((message) => {
-      appendMessage(
-        message.content,
-        message.is_human ? HUMAN_LABEL : PITS_LABEL,
-        container
-      );
+      appendMessage(message.content, message.is_human, container);
     });
   }
 };
@@ -65,17 +62,34 @@ const sendNewMessage = async (content, chat_id, container) => {
   if (!postResponse) return;
   const data = await fetchJson(`/ai/${chat_id}/`);
   if (data) {
-    appendMessage(data.content, PITS_LABEL, container, true);
+    appendMessage(data.content, false, container, true);
   }
 };
 
 const hideInputArea = () => {
   document.getElementById("inputArea").style.display = "none";
-}
+};
 
 const showInputArea = () => {
   document.getElementById("inputArea").style.display = "";
-}
+};
+
+const scrollToBottom = () => {
+  const wrapper = document.getElementById("wrapper");
+  wrapper.scrollTo(0, wrapper.scrollHeight);
+};
+
+const scrollObserver = (wrapper) => {
+  const observer = new MutationObserver((mutations) => {
+    scrollToBottom();
+  });
+
+  observer.observe(wrapper, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+  });
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const wrapper = document.getElementById("wrapper");
@@ -83,6 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const messages = document.getElementById("messages");
   const cursor = document.getElementsByClassName("cursor")[0];
   const chat_id = getChatId();
+  scrollObserver(wrapper);
   getMessages(chat_id, messages);
   wrapper.focus();
 
@@ -94,13 +109,14 @@ document.addEventListener("DOMContentLoaded", () => {
       currentText = currentText.trim();
       if (!currentText) return;
       hideInputArea();
-      appendMessage(currentText, HUMAN_LABEL, messages);
+      appendMessage(currentText, true, messages, false);
       sendNewMessage(currentText, chat_id, messages).then(() => {
         currentText = "";
         textInput.textContent = HUMAN_LABEL;
         textInput.appendChild(cursor);
       });
     } else if (event.key === "Backspace") {
+      if (textInput.textContent.length <= HUMAN_LABEL.length) return;
       textInput.textContent = textInput.textContent.slice(0, -1);
       currentText = currentText.slice(0, -1);
       textInput.appendChild(cursor);
@@ -109,6 +125,5 @@ document.addEventListener("DOMContentLoaded", () => {
       currentText += event.key;
       textInput.appendChild(cursor);
     }
-    wrapper.scrollTo(0, wrapper.scrollHeight);
   });
 });
